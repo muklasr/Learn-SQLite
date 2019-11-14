5c9d262b0dc94be4999ed6f690f54170d7f1e42a
muklasr <muclasrachmanto@gmail.com>
2019-11-14 07:56:13 +0700

    embuh lali

--- app/src/main/java/com/muklas/learnsqlite/MainActivity.java
+++ app/src/main/java/com/muklas/learnsqlite/MainActivity.java
@@ -1,13 +1,41 @@
 package com.muklas.learnsqlite;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    ArrayList<Contact> list = new ArrayList<>();
+    private DBHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dbHelper = new DBHelper(this);
+        recyclerView = findViewById(R.id.recyclerView);
+
+        loadData();
+
+    }
+
+    private void loadData(){
+        String query = "SELECT * FROM sqlite";
+        Cursor cursor = dbHelper.getReadableDatabase().rawQuery(query,  null);
+        for (int i=0; i<cursor.getCount(); i++){
+            cursor.moveToPosition(i);
+            Contact contact = new Contact(Integer.parseInt(cursor.getString(0)), cursor.getString(1), cursor.getString(2), cursor.getString(3));
+            list.add(contact);
+        }
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new ListAdapter(list));
     }
 }
