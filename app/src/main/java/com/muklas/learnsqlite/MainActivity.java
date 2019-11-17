577cf4787d94290cbef7631fb9dddc3c6762885f
muklasr <muclasrachmanto@gmail.com>
2019-11-17 10:13:24 +0700

    lali

--- app/src/main/java/com/muklas/learnsqlite/MainActivity.java
+++ app/src/main/java/com/muklas/learnsqlite/MainActivity.java
@@ -1,32 +1,47 @@
 package com.muklas.learnsqlite;
 
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
-    ArrayList<Contact> list = new ArrayList<>();
-    private DBHelper dbHelper;
+    public MainActivity ma;
+    public ListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        dbHelper = new DBHelper(this);
+        ma = this;
         recyclerView = findViewById(R.id.recyclerView);
+        FloatingActionButton btnAdd = findViewById(R.id.btnAdd);
 
         loadData();
 
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                BottomSheetAdd bottomSheetAdd = new BottomSheetAdd(null, 0);
+                bottomSheetAdd.show(fragmentManager, "TAG");
+            }
+        });
     }
 
     private void loadData(){
+        ArrayList<Contact> list = new ArrayList<>();
+        DBHelper dbHelper = new DBHelper(getApplicationContext());
         String query = "SELECT * FROM sqlite";
         Cursor cursor = dbHelper.getReadableDatabase().rawQuery(query,  null);
         for (int i=0; i<cursor.getCount(); i++){
@@ -36,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(new ListAdapter(list));
+        adapter = new ListAdapter(list);
+        recyclerView.setAdapter(adapter);
     }
 }
